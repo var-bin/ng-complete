e9a2a93aacf40c82d2c3a17b780b4d76aa4e203b
var-bin <var-bin@users.noreply.github.com>
2018-12-11 20:49:15 +0200

    Work with components and various data bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-complete';
+  title = 'Ng Complete';
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
+  serverId = 10;
+  serverStatus = 'offline';
+  allowToAdd = true;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test server name';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.allowToAdd = !this.allowToAdd;
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
