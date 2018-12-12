3fa6893e0ff615d37971034c3165226629ac456b
var-bin <var-bin@users.noreply.github.com>
2018-12-12 16:28:07 +0200

    Start a new app from scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServerStatusComponent } from './server/server-status/server-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServerStatusComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
