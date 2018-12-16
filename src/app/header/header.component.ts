c9ec1e762dde117eb599d5adbc7a2237d1b8f5f6
var-bin <var-bin@users.noreply.github.com>
2018-12-16 21:56:12 +0200

    Implement login for showing recipe book or shopping list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  isRecipeBookMode: boolean = true;
 
+  @Output() recipeBookMode = new EventEmitter<boolean>();
+
+  onRecipeBook(event: MouseEvent) {
+    event.preventDefault();
+
+    if (!this.isRecipeBookMode) {
+      this.isRecipeBookMode = true;
+
+      this.recipeBookMode.emit(this.isRecipeBookMode);
+    }
+  }
+
+  onShoppingList(event: MouseEvent) {
+    event.preventDefault();
+
+    if (this.isRecipeBookMode) {
+      this.isRecipeBookMode = false;
+
+      this.recipeBookMode.emit(this.isRecipeBookMode);
+    }
+  }
 }
