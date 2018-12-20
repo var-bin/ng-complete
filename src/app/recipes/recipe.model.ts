4b37f1917145fea54f4164c5676ceabbe5eeace8
var-bin <var-bin@users.noreply.github.com>
2018-12-20 20:57:03 +0200

    Add new field `ingredients`. Refactoring

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,10 @@
-export class Recipe {
-  name: string;
-  description: string;
-  imagePath: string;
+import { Ingredient } from '../shared/models/index';
 
-  constructor(name: string, desc: string, imagePath: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
-  }
+export class Recipe {
+  constructor(
+    public name: string,
+    public description: string,
+    public imagePath: string,
+    public ingredients: Ingredient[]
+  ) {}
 }
