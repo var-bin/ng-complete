1d8e9283ef72022ce056d3c9f9237dbe1a43454c
var-bin <var-bin@users.noreply.github.com>
2018-12-22 20:25:25 +0200

    Refactoring: Rename `RecipeDetailComponent` -> `RecipeItemDetailComponent`. Replace it to `./recipes/recipe-list/recipe-item-detail/`

--- src/app/recipes/recipe-list/recipe-item-detail/recipe-item-detail.component.ts
+++ src/app/recipes/recipe-list/recipe-item-detail/recipe-item-detail.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.scss']
+  templateUrl: './recipe-item-detail.component.html',
+  styleUrls: ['./recipe-item-detail.component.scss']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeItemDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
   constructor(private recipeService: RecipeService) { }
