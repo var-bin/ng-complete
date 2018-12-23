0ff8ed59e2bd7c98046948233b41309cfbfea59d
var-bin <var-bin@users.noreply.github.com>
2018-12-23 22:23:47 +0200

    Add new route `recipe-book/:id`

--- src/app/app-routes.config.ts
+++ src/app/app-routes.config.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found/page-not-found.component';
+import { RecipeItemDetailComponent } from './recipes/recipe-list/recipe-item-detail/recipe-item-detail.component';
 
 export const appRoutes: Routes = [
   {
@@ -12,6 +13,10 @@ export const appRoutes: Routes = [
   {
     path: 'recipe-book',
     component: RecipesComponent
+  },
+  {
+    path: 'recipe-book/:id',
+    component: RecipeItemDetailComponent
   },
   {
     path: '',
