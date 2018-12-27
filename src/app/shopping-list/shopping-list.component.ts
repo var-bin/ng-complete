042cb33ef61b3e40cf2e344f55ba9373a7f2e011
var-bin <var-bin@users.noreply.github.com>
2018-12-27 18:01:44 +0200

    Add `shoppingLists` prop

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router, NavigationExtras } from '@angular/router';
 
-import { Ingredient } from '../shared/models';
-
-import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../shared/models';
+import { ShoppingList } from './shopping-list.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -15,6 +16,7 @@ import { Subscription } from 'rxjs';
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   addNewItemForm: FormGroup;
+  shoppingLists: ShoppingList[];
 
   private ingredientsSubscription: Subscription;
 
@@ -25,6 +27,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getAllIngredients();
+    this.shoppingLists = this.shoppingListService.getAllShoppingLists();
     this.addNewItemForm = new FormGroup({
       itemName: new FormControl('', [
         Validators.required,
