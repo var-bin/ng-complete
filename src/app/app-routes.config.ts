6e6991822d2b84c92218ca2f3fbc4ccfc54b181a
var-bin <var-bin@users.noreply.github.com>
2019-01-06 13:27:21 +0200

    Add `children` routes for 'recipe-book'. Refactoring import routes

--- src/app/app-routes.config.ts
+++ src/app/app-routes.config.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found/page-not-found.component';
-import { RecipeItemDetailComponent } from './recipes/recipe-list/recipe-item-detail/recipe-item-detail.component';
+import { PageNotFoundComponent } from './shared/components';
+import { ShoppingListComponent } from './shopping-list';
+
+import { RecipeItemDetailComponent } from './recipes/recipe-list';
+import { RecipeItemEditComponent } from './recipes/recipe-list';
+import { RecipesComponent } from './recipes';
+
 
 export const appRoutes: Routes = [
   {
@@ -17,6 +20,10 @@ export const appRoutes: Routes = [
       {
         path: ':id',
         component: RecipeItemDetailComponent
+      },
+      {
+        path: ':id/edit',
+        component: RecipeItemEditComponent
       }
     ]
   },
