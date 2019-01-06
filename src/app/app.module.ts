a39fd7b1ca29bcec6ff83f2cd2b177513d8e7880
var-bin <var-bin@users.noreply.github.com>
2019-01-06 13:27:54 +0200

    Refactoring import routes. Add `RecipeItemEditComponent`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemDetailComponent } from './recipes/recipe-list/recipe-item-detail/recipe-item-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListItemComponent } from './shopping-list/shopping-list-item/shopping-list-item.component';
+
+import { RecipesComponent } from './recipes';
+import { RecipeService } from './recipes';
+import { RecipeListComponent } from './recipes/recipe-list';
+import { RecipeItemDetailComponent } from './recipes/recipe-list';
+import { RecipeItemComponent } from './recipes/recipe-list';
+import { RecipeItemActionButtonComponent } from './recipes/recipe-list';
+import { RecipeItemEditComponent } from './recipes/recipe-list';
+
+import { ShoppingListComponent } from './shopping-list';
+import { ShoppingEditComponent } from './shopping-list';
+import { ShoppingListItemComponent } from './shopping-list';
+import { ShoppingListService } from './shopping-list';
+
+import { PageNotFoundComponent } from './shared/components';
 import { BetterHighlightDirective } from './shared/directives/better-highlight.directive';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found/page-not-found.component';
-import { RecipeItemActionButtonComponent } from './recipes/recipe-list/recipe-item-action-button/recipe-item-action-button.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +36,8 @@ import { RecipeItemActionButtonComponent } from './recipes/recipe-list/recipe-it
     ShoppingListItemComponent,
     BetterHighlightDirective,
     PageNotFoundComponent,
-    RecipeItemActionButtonComponent
+    RecipeItemActionButtonComponent,
+    RecipeItemEditComponent
   ],
   imports: [
     BrowserModule,
