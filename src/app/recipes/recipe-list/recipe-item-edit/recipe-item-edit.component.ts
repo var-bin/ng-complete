cf39b2b9306f419d0c064dfb16f7145c572c4be3
var-bin <var-bin@users.noreply.github.com>
2019-01-08 18:01:02 +0200

    Implement basic reactive form logic

--- src/app/recipes/recipe-list/recipe-item-edit/recipe-item-edit.component.ts
+++ src/app/recipes/recipe-list/recipe-item-edit/recipe-item-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-item-edit',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item-edit.component.scss']
 })
 export class RecipeItemEditComponent implements OnInit {
+  editRecipeForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.editRecipeForm = new FormGroup({
+      itemTitle: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      itemImage: new FormControl('', [
+        Validators.required,
+        Validators.min(1)
+      ]),
+      itemDescription: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      itemIngredients: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ])
+    });
   }
 
+  onSubmit(): void {
+    console.log('onSubmit works');
+  }
 }
