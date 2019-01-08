dec914a62bb6380456e4b651e716e2d356dbb23c
var-bin <var-bin@users.noreply.github.com>
2019-01-08 16:41:31 +0200

    Add `canActivateChild` guard

--- src/app/recipes/recipe-route-activator.service.ts
+++ src/app/recipes/recipe-route-activator.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  CanActivateChild
+} from '@angular/router';
+
+import { of, Observable } from 'rxjs';
 
 import { RecipeService } from './recipe.service';
 import { routes } from '../routes.enum';
@@ -7,14 +14,13 @@ import { routes } from '../routes.enum';
 @Injectable({
   providedIn: 'root'
 })
-export class RecipeRouteActivatorService implements CanActivate {
-
+export class RecipeRouteActivatorService implements CanActivate, CanActivateChild {
   constructor(
     private router: Router,
     private recipeService: RecipeService
   ) { }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     // Example of URL: /recipe-book/recipe-book-1
     const recipeUrlParts: string[] = route.params['id'].split('-');
     const recipeId: number = +recipeUrlParts[recipeUrlParts.length - 1];
@@ -24,6 +30,10 @@ export class RecipeRouteActivatorService implements CanActivate {
       this.router.navigate([routes.PAGE_NOT_FOUND_REDIRECT]);
     }
 
-    return true;
+    return of(true);
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.canActivate(route);
   }
 }
