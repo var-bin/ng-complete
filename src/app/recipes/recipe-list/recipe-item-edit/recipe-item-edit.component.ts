8dc9d5712064c9b72f81bb5254798fb5d413d0bb
var-bin <var-bin@users.noreply.github.com>
2019-01-10 11:58:49 +0200

    Implement logic for `RecipeItemEditComponent`. displaying data for the current recipe

--- src/app/recipes/recipe-list/recipe-item-edit/recipe-item-edit.component.ts
+++ src/app/recipes/recipe-list/recipe-item-edit/recipe-item-edit.component.ts
@@ -1,38 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+import { Recipe } from '../../recipe.model';
+import { Ingredient } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-recipe-item-edit',
   templateUrl: './recipe-item-edit.component.html',
   styleUrls: ['./recipe-item-edit.component.scss']
 })
-export class RecipeItemEditComponent implements OnInit {
+export class RecipeItemEditComponent implements OnInit, OnDestroy {
   editRecipeForm: FormGroup;
+  editedRecipe: Recipe;
 
-  constructor() { }
+  private routeDataSubscription$: Subscription;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.routeDataSubscription$ = this.route.data
+      .subscribe((data: { recipe: Recipe }) => {
+        this.editedRecipe = data.recipe;
+      });
+
     this.editRecipeForm = new FormGroup({
-      itemTitle: new FormControl('', [
+      recipeTitle: new FormControl(this.editedRecipe.name, [
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(5)
       ]),
-      itemImage: new FormControl('', [
-        Validators.required,
-        Validators.min(1)
+      recipeImage: new FormControl(this.editedRecipe.imagePath, [
+        Validators.required
       ]),
-      itemDescription: new FormControl('', [
+      recipeDescription: new FormControl(this.editedRecipe.description, [
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(10)
       ]),
-      itemIngredients: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3)
-      ])
+      recipeIngredients: new FormGroup(
+        this.mapIngredients<Ingredient>(this.editedRecipe.ingredients)
+      )
     });
+
+    this.mapIngredients<Ingredient>(this.editedRecipe.ingredients);
   }
 
   onSubmit(): void {
-    console.log('onSubmit works');
+    console.log('onSubmit works', this.editRecipeForm.value);
+  }
+
+  ngOnDestroy() {
+    this.routeDataSubscription$.unsubscribe();
+  }
+
+  getIngredientName(key: string, index: number): string {
+    return `ingredient${key[0].toUpperCase()}${key.slice(1)}_${index}`;
+  }
+
+  private mapIngredients<T>(ingredients: T[]) {
+    const formControls: {[key: string]: FormControl} = {};
+
+    ingredients.forEach((ingredient: T, index: number) => {
+      for (const key in ingredient) {
+        // Don't use `ingredient.hasOwnProperty(key)` directly. It may be overwritten
+        if ({}.hasOwnProperty.call(ingredient, key)) {
+          const ingredientName = this.getIngredientName(key, index);
+
+          formControls[`${ingredientName}`] = new FormControl(ingredient[key], [
+            Validators.required
+          ]);
+        }
+      }
+    });
+
+    return formControls;
   }
 }
