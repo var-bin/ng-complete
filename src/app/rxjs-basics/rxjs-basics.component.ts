6f5d037472c55bceaf9988779f7473fee8029d9b
var-bin <var-bin@users.noreply.github.com>
2019-01-15 16:36:53 +0200

    Practice with `interval`, `takeUntil`, `filter`

--- src/app/rxjs-basics/rxjs-basics.component.ts
+++ src/app/rxjs-basics/rxjs-basics.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { interval } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-rxjs-basics',
   templateUrl: './rxjs-basics.component.html',
@@ -10,6 +13,19 @@ export class RxjsBasicsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.firstTenNumbers();
   }
 
+  private firstTenNumbers() {
+    const myNumbers = interval(1000);
+    const firstTenNumbers = myNumbers.pipe(filter(n => n > 10));
+
+    myNumbers
+      .pipe(
+        takeUntil(firstTenNumbers)
+      )
+      .subscribe((n: number) => {
+        console.log(`number: ${n}`);
+      });
+  }
 }
